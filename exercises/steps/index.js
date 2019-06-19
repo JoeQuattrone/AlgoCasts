53349171e11749413064fb3ee923360b8d1e0ffe
Joe Quattrone <joequattrone5@gmail.com>
2019-06-19 11:58:21 -0400

    refactor of original solution

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -21,13 +21,7 @@ function steps(n) {
   str = ''
   for (let i = 0; i < n ; i++ ) {
     str += ' '
-  }
-
-  let counter = 0
-  const hash = '#'
-  while (counter < n) {
     str = hash + str.slice(0, -1)
-    counter++
     console.log(str)
   }
 }
